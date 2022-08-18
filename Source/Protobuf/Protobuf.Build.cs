2413b219cc2066f30cd348202119bc00c42b63cc
Sang Tian <indiestian@gmail.com>
2022-08-18 18:19:45 +0800

    Revert "upgrade to protobuf v3.21.5"

--- Source/Protobuf/Protobuf.Build.cs
+++ Source/Protobuf/Protobuf.Build.cs
@@ -4,16 +4,72 @@ using UnrealBuildTool;
 using System.IO;
 public class Protobuf : ModuleRules
 {
+    private string ModulePath
+    {
+        get { return ModuleDirectory; }
+    }
+
+    private string ThridPartyPath
+    {
+        get { return Path.Combine(ModulePath, "ThirdParty/"); }
+    }
 
     public Protobuf(ReadOnlyTargetRules Target) : base(Target)
-    {
-        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+		
+		PublicIncludePaths.AddRange(
+			new string[] {
+				// ... add public include paths required here ...
+			}
+			);
+				
+		
+		PrivateIncludePaths.AddRange(
+			new string[] {
+				// ... add other private include paths required here ...
+			}
+			);
+			
+		
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+				// ... add other public dependencies that you statically link with here ...
+			}
+			);
+			
+		
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				// ... add private dependencies that you statically link with here ...	
+			}
+			);
+		
+		
+		DynamicallyLoadedModuleNames.AddRange(
+			new string[]
+			{
+				// ... add any modules that your module loads dynamically here ...
+			}
+			);
 
-        PublicDependencyModuleNames.AddRange(new string[] { "Core" });
+        PublicSystemIncludePaths.AddRange(
+            new string[]
+            {
+                Path.Combine(ThridPartyPath,"protobuf/include")
+            }
+            );
 
-        PublicSystemIncludePaths.AddRange(new string[] { Path.Combine(ModuleDirectory, "ThirdParty") });
+        PublicAdditionalLibraries.AddRange(
+            new string[]
+            {
+            }
+            );
 
-        if (Target.bForceEnableRTTI)
+        if(Target.bForceEnableRTTI)
         {
             bUseRTTI = true;
             PublicDefinitions.Add("GOOGLE_PROTOBUF_NO_RTTI=0");
@@ -23,16 +79,16 @@ public class Protobuf : ModuleRules
             bUseRTTI = false;
             PublicDefinitions.Add("GOOGLE_PROTOBUF_NO_RTTI=1");
         }
+        if(Target.Platform != UnrealTargetPlatform.Win32 && Target.Platform != UnrealTargetPlatform.Win64)
+        {
+            PublicDefinitions.Add("HAVE_PTHREAD");
+        }
 
-        PublicDefinitions.AddRange(
-            new string[] {
-                "LANG_CXX11",
-                "PROTOBUF_USE_DLLS", // Basically for ThreadCache
-            }
-        );
+        PublicDefinitions.Add("_CRT_SECURE_NO_WARNINGS");
+        PublicDefinitions.Add("LANG_CXX11");
 
         ShadowVariableWarningLevel = WarningLevel.Off;
         bEnableUndefinedIdentifierWarnings = false;
-
+        bEnableExceptions = true;
     }
 }
